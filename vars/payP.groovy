f44ea7f3f38043bdb1089294d3546980541e0470
Shailendra Singh <b4shailen@gmail.com>
2024-05-11 02:10:21 +0530

    Update and rename paymentservicePipeline.groovy to payP.groovy

---
+++ vars/payP.groovy
@@ -0,0 +1,10 @@
+def pay() {
+    stage('Hello Paymentservice!') {
+        steps {
+            echo "Step: Executing paymentservicePipeline() method..."
+        }
+    }
+    
+ // Add other stages as needed
+}
+
