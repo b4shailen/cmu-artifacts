7e3939313e2dc1d29f445735ed4dfcf3ea693f93
Ubuntu <ubuntu@ip-172-31-7-178.ap-south-1.compute.internal>
2024-05-11 18:45:41 +0000

    Changed read tags from git instead of docker hub

--- Update/image_scanner_updater.py
+++ Update/image_scanner_updater.py
@@ -6,205 +6,161 @@ import sys
 import datetime
 import re
 from datetime import datetime
+from pathlib import Path
 
-tagfilelocation = "/home/ubuntu/imageupdater"
-tagfile = "tagfile"
-knowntagfile = "knowntags.txt"
-scanlogslocation = "/home/ubuntu/imageupdater/scanlogs"
 # Define the format of the datetime strings
 datetime_format = "%Y-%m-%d %H:%M:%S"
-#microserv = ['adservice', 'checkoutservice', 'productcatalogservice', 'emailservice', 'cartservice', 'frontend', 'currencyservice', 'shippingservice', 'recommendationservice', 'paymentservice', 'loadgenerator']
-microserv = ['shippingservice']
+scanloglocation = "/home/ubuntu//scanlogs"
+scanscorethreshold = 0
+servsummary = []
+manifestlocation = "/home/ubuntu/cmu-artifacts/Deploy/manifests/"
+microserv = []
 
-#Make a list of current tags of the microservices running
-print('\n###############################################################')
-print('Reading current tagfile and making a list of tags in production')
-print('###############################################################')
-production_tags = []
-with open(os.path.join(tagfilelocation,tagfile)) as f:
-    for line in f:
-        line = line.split(',')
-        production_tags.append(line)
-        
-print('\n###############################################################')
-print('Making list of knowntags to compare with images in docker hub')
-print('###############################################################')
-known_tags = []
-with open(os.path.join(tagfilelocation,knowntagfile)) as f:
-    for line in f:
-        line = line.split(',')
-        known_tags.append(line)
+print('##########################################################')
+print('Image Update beginning')
+print('##########################################################\n')
 
-upgradelist = []
-updatedtaglist = []
-nowknown_tags = copy.deepcopy(known_tags)
+print('##########################################################')
+print('Check for new tags in git')
+print('##########################################################\n')
+changeserv = []
+command = "git -C " + manifestlocation + " pull"
+stream = os.popen(command)
+output = stream.read()
+output = output.split('\n')
+print(output)
+for line in range(len(output)):
+    if re.match(r' Deploy\/manifests\/\w+_rollout.yaml.*', output[line], re.M | re.I):
+        servcha = re.search(r' Deploy\/manifests\/(\w+)_rollout.yaml.*', output[line], re.M | re.I)
+        if servcha.group(1) != 'shoppingassistantservice':
+            microserv.append(servcha.group(1))
 
-for service in range(len(microserv)):
-    tag_list = []
-    servtags = []
-    deltatags = []
-    foundtag = []
-    prodservtag = ''
-    prodservts = ''
-    foundflag = 0
-    tagcount = 1
-    print('\n##########################################################')
-    print('Checking %s' % microserv[service])
-    print('##########################################################')
-    #Find the current tag and timestamp for the microservice
-    for tags in range(len(production_tags)):
-        if production_tags[tags][0] == microserv[service]:
-            prodservtag = production_tags[tags][1]
-            prodservts = production_tags[tags][2]
-            prodservts = datetime.strptime(prodservts, datetime_format)
+if len(microserv) != 0:
+    print('Following services have new tags and will be update.\n')
+    for serv1 in range(len(microserv)):
+        print('%s', microserv[serv1])
 
-    #Load the known tags for this service
-    for ktags in range(len(nowknown_tags)):
-        if nowknown_tags[ktags][0] == microserv[service]:
-            nowknown_tags[ktags].pop(0)
-            nowknown_tags[ktags].pop()
-            servtags = nowknown_tags[ktags]
-
-    #Capture all the tags for each microservice
-    command = 'skopeo list-tags --creds cmupro7:grp5pro7cmu@1234 docker://cmupro7/' + microserv[service]
-    stream = os.popen(command)
-    output = stream.read() 
-    output = output.split('\n')
-    #Create a list of the tags
-    for line in range(len(output)):
-        if re.match(r'^\s+\"\d+\"(\,)?', output[line], re.M | re.I):
-            tag = re.search(r'^\s+\"(\d+)\"(\,)?', output[line], re.M | re.I)
-            foundtag.append(tag.group(1))
-    #compare foundtags and new tags to find delta
-    deltatags = [x for x in foundtag if x not in servtags]
-    
-    if(len(deltatags)>0):
-        print('Found %d new tags\n' % len(deltatags))
-        print('Looking for the latest tag\n')
-        newtimestamp = prodservts
-        newtag = prodservtag
-        #From delta tags find the latest tag, first find timestamp of current production tag
-        for dtags in range(len(deltatags)):
-            command2 = 'skopeo inspect --creds cmupro7:grp5pro7cmu@1234 docker://cmupro7/' + microserv[service] + ':' + deltatags[dtags] + ' |grep Created'
-            #print(command2)
-            stream1 = os.popen(command2)
+    for serv in range(len(microserv)):
+        summary = []
+        print('##########################################################')
+        print('Updating %s' % microserv[serv])
+        print('##########################################################\n')
+        print('Checking the rollout health and image tag for %s\n' % microserv[serv])
+        command = "kubectl argo rollouts get rollout " + microserv[serv] + " -n onlineboutique"
+        stream = os.popen(command)
+        output = stream.read()
+        output = output.split('\n')
+        for line in range(len(output)):
+            if re.match(r'^Status\:.*', output[line], re.M | re.I):
+                if re.match(r'^Status\:.*(Healthy)', output[line], re.M | re.I):
+                    servstat = 1
+                    print("Status of %s is Healthy, Will Check for newer tags in git repo\n"% microserv[serv])
+                    summary = [microserv[serv],'healthy']
+                else:
+                    servstat = 0
+                    print("Status of %s Not Healthy, Moving onto next service\n" % microserv[serv])
+                    summary = [microserv[serv],'unhealthy']
+                    break
+            if re.match(r'^Images\:\s+cmupro7/\w+:(\d+)',output[line], re.M | re.I):
+                tagprod = re.search(r'^Images\:\s+cmupro7/\w+:(\d+)',output[line], re.M | re.I)
+                print("Tag in production is %s\n"% tagprod.group(1))
+        if(servstat == 1):
+            print("Finding the new tag number for %s\n" % microserv[serv])
+            command1 = 'grep image ' + manifestlocation + microserv[serv] + '_rollout.yaml'
+            stream1 = os.popen(command1)
             output1 = stream1.read()
             output1 = output1.split('\n')
-            if re.match(r'^\s+\"Created\"\: \"\d+\-\d+\-\d+T\d+\:\d+\:\d+\.\d+Z', output1[0], re.M | re.I):
-                temptimestamp = re.search(r'^\s+\"Created\"\: \"(\d+\-\d+\-\d+)T(\d+\:\d+\:\d+)\.\d+Z', output1[0], re.M | re.I)
-                temptimestamp = temptimestamp[1] + " " + temptimestamp[2]
-                foundtimestamp = datetime.strptime(temptimestamp, datetime_format)
-                #print("##%s" % foundtimestamp)
-                if foundtimestamp > newtimestamp:
-                    newtimestamp = foundtimestamp
-                    newtag = deltatags[dtags]
-        print("Latest tag is %s with timestamp %s\n" % (newtag, newtimestamp))
-        print("Exeucting Docker bench security scan on tag %s\n"% newtag)
-        command3 = "docker pull cmupro7/" + microserv[service] + ':' + deltatags[dtags]
-        stream3 = os.popen(command3)
-        output3 = stream3.read()
-        output3 = output3.split('\n')
-        command4 = "docker images"
-        stream4 = os.popen(command4)
-        output4 = stream4.read()
-        output4 = output4.split('\n')
-        if re.match(r'^cmupro7/\w+\s+\d+\s+(\w+)\s+', output4[1], re.M | re.I):
-            imageid = re.search(r'cmupro7/\w+\s+\d+\s+(\w+)\s+', output4[1], re.M | re.I)
-        logtimestamp =  datetime.now().strftime("%Y%m%d_%H%M%S")
-        command5 ="sudo /home/ubuntu/docker-bench-security/docker-bench-security.sh -l /home/ubuntu/imageupdater/scanlogs/scanlog_" + microserv[service] + logtimestamp + ".log"
-        stream5 = os.popen(command5)
-        output5 = stream5.read()
-        output5 = output5.split('\n')
-        command6 = "docker rmi " + imageid[1]
-        stream6 = os.popen(command6)
-        print("Docker bench security scans complete, Please review logs at /home/ubuntu/scanlogs/scanlog_%s%s\n" % (microserv[service], logtimestamp))
-        print("Performing Canary Upgrade for %s\n" % microserv[service])
-        command7 = "kubectl argo rollouts set image rollouts-demo rollouts-demo=argoproj/" + microserv[service] + ":" + deltatags[dtags]
-        stream7 = os.popen(command7)
-        output7 = stream7.read()
-        output7 = output7.split('\n')
-        if re.match(r'^.*image update', output7[0], re.M | re.I):
-            print('%s was upgraded successfully'% microserv[service])
-            upgradelist.append([microserv[service], newtag, newtimestamp])
-            #print(foundtag)
-            foundtag.insert(0,microserv[service])
-            updatedtaglist.append(foundtag)
-        else:
-            print('%s upgrade failed'% microserv[service])
-            upgradelist.append([microserv[service], newtag, newtimestamp])
-            #print(foundtag)
-            #foundtag.insert(0,microserv[service])
-            #updatedtaglist.append(foundtag)
-            #print(updatedtaglist)
-            #foundflag = 1
-    else:
-        print("No new tags found for %s" % microserv[service])
-
-#Update tagfile
-if len(upgradelist) > 0:
-    #Before upgrading archive the currrent tagfile
-    print('\n##########################################################')
-    print('Archiving current tagfile')
-    print('##########################################################')
-    archivecommand = 'sudo mv tagfile /home/ubuntu/imageupdater/archive/tagfile_' + datetime.now().strftime("%Y%m%d_%H%M%S")
-    archivestream = os.popen(archivecommand)
-    archiveoutput = archivestream.read()
-    #print(archiveoutput)
-    #Update the services which have been modified
-    for newtags in range(len(upgradelist)):
-        for oldtags in range(len(production_tags)):
-            if upgradelist[newtags][0] == production_tags[oldtags][0]:
-                if upgradelist[newtags][1] != production_tags[oldtags][1]:
-                    production_tags[oldtags][1] = upgradelist[newtags][1]
-                    production_tags[oldtags][2] = upgradelist[newtags][2]
-    print('\n##########################################################')
-    print('Creating new tagfile')
-    print('##########################################################')
-    tagfilehandle = open("tagfile", "w")
-    for i in range(len(production_tags)):
-        tagfilehandle.write("%s,%s,%s,%s"%(production_tags[i][0],production_tags[i][1],production_tags[i][2],production_tags[i][3]))
-    tagfilehandle.close()
-else:
-    print('\n##########################################################')
-    print('Tagfile doesnt need to change as no services were upgraded')
-    print('##########################################################')
-
-#Update knowntagfile
-if len(updatedtaglist) > 0:
-    #Before updating archive the current knowntagfile
-    print('\n##########################################################')
-    print('Archiving the current knowntagfile')
-    print('##########################################################')
-    archivecommand = 'sudo mv knowntags.txt /home/ubuntu/imageupdater/archive/knwontag_' + datetime.now().strftime("%Y%m%d_%H%M%S")
-    archivestream = os.popen(archivecommand)
-    archiveoutput = archivestream.read()
-
-    #Update the services which have been successfully upgraded
-    for olddata in range(len(known_tags)):
-        #print(known_tags[olddata][0])
-        for newdata in range(len(updatedtaglist)):
-            #print(updatedtaglist[newdata][0])
-            if known_tags[olddata][0] == updatedtaglist[newdata][0]:
-                known_tags[olddata] = updatedtaglist[newdata]
-                known_tags[olddata].append('\n')
-    print('\n##########################################################')
-    print('Creating new knowntagfile')
-    print('##########################################################')
-    tagfilehandle = open("knowntags.txt", "w")
-    for i in range(len(known_tags)):
-        for j in range(len(known_tags[i])):
-            if j == len(known_tags[i]) - 1 :
-                tagfilehandle.write("%s"% known_tags[i][j])
+            taggitlist = []
+            #Create a list of the tags
+            for line in range(len(output1)):
+                if re.match(r'^\s+image\: cmupro7/\w+:\d+', output1[line], re.M | re.I):
+                    tagsko = re.search(r'^\s+image\: cmupro7/\w+:(\d+)', output1[line], re.M | re.I)
+                    taggitlist.append(int(tagsko.group(1)))
+            taggit = max(taggitlist)
+            print("The latest tag in git for %s is %d\n" %(microserv[serv], taggit))
+        scanphase = 1
+        if(scanphase == 1):
+            print("Exeucting Docker bench security scan on tag %s\n" % taggit)
+            command3 = "docker pull cmupro7/" + microserv[serv] + ':' + str(taggit)
+            stream3 = os.popen(command3)
+            output3 = stream3.read()
+            output3 = output3.split('\n')
+            command4 = "docker images"
+            stream4 = os.popen(command4)
+            output4 = stream4.read()
+            output4 = output4.split('\n')
+            if re.match(r'^cmupro7/\w+\s+\d+\s+(\w+)\s+', output4[1], re.M | re.I):
+                imageid = re.search(r'cmupro7/\w+\s+\d+\s+(\w+)\s+', output4[1], re.M | re.I)
+            logtimestamp =  datetime.now().strftime("%Y%m%d_%H%M%S")
+            logfilename = scanloglocation + "/scanlog_" + microserv[serv] + logtimestamp + ".log"
+            jsonlogfilename = logfilename + ".json"
+            command5 ="sudo /home/ubuntu/docker-bench-security/docker-bench-security.sh -l " + logfilename 
+            stream5 = os.popen(command5)
+            output5 = stream5.read()
+            output5 = output5.split('\n')
+            command6 = "docker rmi " + imageid[1]
+            stream6 = os.popen(command6)
+            print("Docker bench security scans complete, Please review logs at %s /scanlog_%s%s\n" % (scanloglocation, microserv[serv], logtimestamp))
+            if Path(jsonlogfilename).is_file():
+                print("Checking Scan scores\n")
+                with open(jsonlogfilename) as f:
+                    for line in f:
+                        #line = line.split(',')
+                        if re.match(r'^\s+\"score\"\:\s+(\d+)\,', line, re.M | re.I):
+                            score = re.search(r'^\s+\"score\"\:\s+(\d+)\,', line, re.M | re.I)
+                            if(int(score.group(1)) > scanscorethreshold):
+                                print("Scan score %s meets threshold of %d, Moving to upgrade\n" % (int(score.group(1)), scanscorethreshold))
+                                summary = [microserv[serv],'upgradephase']
+                                upgradephase =1
+                            else:
+                                print("Scan score %s doesnt meet threshold of %d, Review the scanlogs, Moving to next service\n" % (int(score.group(1)), scanscorethreshold))
+                                upgradephase = 0
+                                summary = append[microserv[serv],'scanfail']
             else:
-                tagfilehandle.write("%s,"% known_tags[i][j])
-    tagfilehandle.close()
+                print("Couldnt check scan score,Skipping upgrade, moving to next service\n")
+                upgradephase = 0
+                summary = [microserv[serv],'scannotdone']
+            if(upgradephase == 1):
+                command7 = "kubectl argo rollouts set image " + microserv[serv] + " " + microserv[serv] + "=cmupro7/" + microserv[serv] + ":" + str(taggit) + " -n onlineboutique"
+                stream7 = os.popen(command7)
+                output7 = stream7.read()
+                output7 = output7.split('\n')
+                if re.match(r'^.*image update', output7[0], re.M | re.I):
+                    print('Canary upgrade for %s was successful\n'% microserv[serv])
+                    summary = [microserv[serv],'upgradepass']
+                else:
+                    print('Canary upgrade for %s failed\n'% microserv[serv])
+                    summary = [microserv[serv],'upgradefail']
+    servsummary.append(summary)
+
+print('##########################################################')
+print('Activity Summary')
+print('##########################################################\n')
+if len(servsummary) == 0:
+    print("No new tags were found and hence no services were updated\n")
 else:
-    print('\n###############################################################')
-    print('KnownTagfile doesnt need to change as no services were upgraded')
-    print('############################################################')
+    for stat in range(len(servsummary)):
+        if(servsummary[stat][1] == 'healthy'):
+            print("%s: Found healthy, need to investigate why it wasnt upgrade\n" % servsummary[stat][0])
+        if(servsummary[stat][1] == 'unhealthy'):
+            print("%s: Found unhealthy, Upgrade was skipped\n" % servsummary[stat][0])    
+        if(servsummary[stat][1] == 'needscan'):
+            print("%s: Needed scan, need to investigate why it wasnt upgrade\n" % servsummary[stat][0])
+        if(servsummary[stat][1] == 'equaltag'):
+            print("%s: Running latest tags, no further action was needed\n" % servsummary[stat][0])
+        if(servsummary[stat][1] == 'impropertag'):
+            print("%s: Production tag higher than GIT tag, need to investigate further, Upgrade was skipped\n" % servsummary[stat][0])
+        if(servsummary[stat][1] == 'upgradephase'):
+            print("%s: Needed an upgrade but wasnt, need to investigate why it wasnt upgrade\n" % servsummary[stat][0])
+        if(servsummary[stat][1] == 'scanfail'):
+            print("%s: Docker-bench-security scan score didnt meet threshold, Upgrade was skipped, Review scan logs\n" % servsummary[stat][0])
+        if(servsummary[stat][1] == 'scannotdone'):
+            print("%s: Docker-bench-security scan logs not available, Upgrade was skipped\n" % servsummary[stat][0])  
+        if(servsummary[stat][1] == 'upgradepass'):
+            print("%s: Canary update was successful, promote when upgrade checks out\n" % servsummary[stat][0])
+        if(servsummary[stat][1] == 'upgradefail'):
+            print("%s: Canary update failed, Need to investigate why\n" % servsummary[stat][0])
 
-
-print('\n##########################################################')
-print('Image Updater done')
-print('\n##########################################################')
-
+print('##########################################################')
+print('Image update wrapped up')
+print('##########################################################\n')
